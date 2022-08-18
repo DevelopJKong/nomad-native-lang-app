bf118642ac51ddaa332ccabee0f74012c8596da6
DevelopJKong <ybaram40@gmail.com>
2022-08-18 22:10:35 +0900

    #4.9 Pan Responder 완료

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, { useRef, useState } from "react";
-import { Animated, Dimensions, Pressable } from "react-native";
+import React, { useRef } from "react";
+import { Animated, PanResponder } from "react-native";
 import styled from "styled-components/native";
 
 const Container = styled.View`
@@ -14,47 +14,13 @@ const Box = styled.View`
 `;
 
 const AnimatedBox = Animated.createAnimatedComponent(Box);
-const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 export default function App() {
-  const position = useRef(new Animated.ValueXY({ x: -SCREEN_WIDTH / 2 - 100, y: -SCREEN_HEIGHT / 2 + 100 })).current;
-  const topLeft = Animated.timing(position, {
-    toValue: {
-      x: -SCREEN_WIDTH / 2 + 100,
-      y: -SCREEN_HEIGHT / 2 + 100,
-    },
-    useNativeDriver: false,
-  });
-  const bottomLeft = Animated.timing(position, {
-    toValue: {
-      x: -SCREEN_WIDTH / 2 + 100,
-      y: SCREEN_HEIGHT / 2 - 100,
-    },
-    useNativeDriver: false,
-  });
-  const bottomRight = Animated.timing(position, {
-    toValue: {
-      x: SCREEN_WIDTH / 2 - 100,
-      y: SCREEN_HEIGHT / 2 - 100,
-    },
-    useNativeDriver: false,
-  });
-  const topRight = Animated.timing(position, {
-    toValue: {
-      x: SCREEN_WIDTH / 2 - 100,
-      y: -SCREEN_HEIGHT / 2 + 100,
-    },
-    useNativeDriver: false,
-  });
-
-  const moveUp = () => {
-    Animated.loop(Animated.sequence([topLeft, bottomLeft, bottomRight, topRight])).start();
-  };
-
-  //   const opacityValue = Y_POSITION.interpolate({
-  //     inputRange: [-300, -100, 100, 300],
-  //     outputRange: [1, 0.1, 0.1, 1],
-  //   });
-  //
+  const position = useRef(
+    new Animated.ValueXY({
+      x: 0,
+      y: 0,
+    })
+  ).current;
 
   const borderRadius = position.y.interpolate({
     inputRange: [-300, 300],
@@ -65,19 +31,28 @@ export default function App() {
     outputRange: ["rgb(255,99,71)", "rgb(71,166,255)"],
   });
 
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (_, { dx, dy }) => {
+        position.setValue({
+          x: dx,
+          y: dy,
+        });
+      },
+    })
+  ).current;
+
   return (
     <Container>
-      <Pressable onPress={moveUp}>
-        <AnimatedBox
-          onPress={moveUp}
-          style={{
-            borderRadius,
-            backgroundColor: bgColor,
-            //transform: [{ rotateY: rotation }, { translateY: position.y }, { translateX: position.x }],
-            transform: [...position.getTranslateTransform()],
-          }}
-        />
-      </Pressable>
+      <AnimatedBox
+        {...panResponder.panHandlers}
+        style={{
+          borderRadius,
+          backgroundColor: bgColor,
+          transform: [...position.getTranslateTransform()],
+        }}
+      />
     </Container>
   );
 }
